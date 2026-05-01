629263fdfcce1b16cd2a24f94e145e7699ab9381
maycon1987 <manualdaempresabr@gmail.com>
2026-05-01 19:30:40 -0300

    parser.py

--- app/services/parser.py
+++ app/services/parser.py
@@ -1,10 +1,12 @@
 import pdfplumber
 
-def extrair_texto_pdf(file_path):
+
+def extrair_texto_pdf(file_path: str) -> str:
     texto = ""
 
     with pdfplumber.open(file_path) as pdf:
         for page in pdf.pages:
-            texto += page.extract_text() + "\n"
+            page_text = page.extract_text() or ""
+            texto += page_text + "\n"
 
     return texto
